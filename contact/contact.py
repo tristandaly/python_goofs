39cb8eef41db8d8148796d21bb50a6e68914e0a1
tristandaly <52462203+tristandaly@users.noreply.github.com>
2019-10-11 15:45:21 -0500

    Add contact.py

---
+++ contact/contact.py
@@ -0,0 +1,20 @@
+import time
+
+
+def is_prime(number):
+    if number < 2:
+        return False
+    elif number is 2:
+        return True
+    else:
+        for i in range(2, number):
+            if number % i is 0:
+                return False
+        return True
+
+for i in range(100):
+    if is_prime(i):
+        for value in range(i):
+            print(str(i))
+            time.sleep(1.3)
+            print()
